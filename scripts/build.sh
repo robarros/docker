ada8e514383eae74ca9ee2ba243a23d1206fd058
robarros <20506531+robarros@users.noreply.github.com>
2018-09-04 13:32:54 -0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ cd docker
 export tags=`git describe --abbrev=0 --tags`
 docker build -t 10.0.0.111:5000/web01:$tags -t 10.0.0.111:5000/web01:latest --no-cache .
 docker push 10.0.0.111:5000/web01:latest
-docker push 10.0.0.111:5000/web01:tags
+docker push 10.0.0.111:5000/web01:$tags
