59129a84567127348d6dd0e7d3e7dc82f915a1ac
robarros <20506531+robarros@users.noreply.github.com>
2018-09-04 14:29:45 -0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-git clone https://github.com/robarros/docker.git
-cd docker
 export tags=`git describe --abbrev=0 --tags`
 docker build -t 10.0.0.111:5000/web01:$tags -t 10.0.0.111:5000/web01:latest --no-cache .
 docker push 10.0.0.111:5000/web01:latest
